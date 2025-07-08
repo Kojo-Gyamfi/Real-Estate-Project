6a60bcd3634120dffbdd6f1e9e55517739e6a356
NANA KOJO <nanakojo@NANAs-MacBook-Pro-2.local>
2025-07-07 21:01:05 -0700

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Components/Header';
 import About from './Components/About';
 import Projects from './Components/Projects';
+import Testimonial from './Components/Testimonial';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <About />
       <Projects />
+      <Testimonial />
     </div>
   )
 }
---
+++ src/Components/Testimonial.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Testimonial = () => {
+    return (
+        <div className='container mx-auto py-10 lg:px-32 w-full overflow-hidden' id='Testimonial'>
+            <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Customer
+                <span className='underline underline-offset-4 decoration-1 under font-light'> Testimonials</span>
+            </h1>
+            <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>
+                Real Stories from Those who Found Home With Us
+            </p>
+        </div>
+    )
+}
+
+export default Testimonial
